1825fa6f646b085871a77f0b534f4c5d33a041d5
EgorDyach <tvoysosed33@gmail.com>
2024-01-10 21:46:37 +0300

    last version

--- main.js
+++ main.js
@@ -2,25 +2,79 @@ const groceries = document.getElementsByClassName("groceries")[0]
 const basket = document.getElementById("basket")
 const allItems = document.getElementById("allItems")
 const userInput = document.getElementById("userInput")
-
-basket.addEventListener("click", function() {
-    allItems.innerHTML = "";
+const addBtn = document.getElementById('addBtn')
+const favouritesItems = document.getElementById('favouritesItems')
+let items = [] // content: str, isFavourite: false, isDeleted: false
+basket.addEventListener("click", function () {
+    items = [];
+    render()
 })
 
-userInput.addEventListener("keydown", function(event){
-    if (event.key == "Enter")
+userInput.addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
         addItem();
+    }
 })
 
-function addItem(){
-    var h2 = document.createElement("h2");
-    h2.innerHTML = "— " + userInput.value;
+addBtn.addEventListener('click', function (event) {
+    addItem()
+})
 
-    h2.addEventListener("click", function(){
-        h2.style.textDecoration = "line-through";
+function addItem() {
+    if (userInput.value != '' && userInput.value.length >= 2 && userInput.value.length <= 50) {
+        items.push({ 
+            content: userInput.value, 
+            isFavourite: false, 
+            isDeleted: false, 
+        })
+        userInput.value = "";
+        render()
+    }
+}
+
+function render() {
+    allItems.innerHTML = ''
+    favouritesItems.innerHTML = ''
+    console.log(items)
+    for (let i = 0; i < items.length; i++) {
+        allItems.append(objectToItem(items[i], i))
+        if (items[i].isFavourite) {
+            favouritesItems.append(objectToItem(items[i], i))
+        }
+    }
+
+}
+
+function objectToItem(obj, i) {
+    const li = document.createElement('li')
+    const deleteBtn = document.createElement('button')
+    const favouriteBtn = document.createElement('button')
+    li.innerHTML = obj.content
+    deleteBtn.classList = 'deleteBtn'
+    deleteBtn.textContent = 'Вычеркнуть'
+    if (obj.isDeleted) {
+        li.classList = 'line'
+    }
+    deleteBtn.addEventListener('click', function () {
+        if (obj.isDeleted == false) {
+            items[i].isDeleted = true
+        } else {
+            items[i].isDeleted = false
+        }
+        render()
     })
-
-    allItems.insertAdjacentElement("beforeend", h2);
-
-    userInput.value = " ";
+    favouriteBtn.classList = 'favourites'
+    if (obj.isFavourite) {
+        favouriteBtn.classList = 'favourites isFavourite'
+    }
+    favouriteBtn.addEventListener('click', function () {
+        if (obj.isFavourite == false) {
+            items[i].isFavourite = true
+        } else {
+            items[i].isFavourite = false
+        }
+        render()
+    })
+    li.append(deleteBtn, favouriteBtn)
+    return li
 }
